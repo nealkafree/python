24cefa45db62df54df122c91c37f29232b12d5fd
kafree <kirillov.bot.daniil@yandex.ru>
2019-06-15 10:46:28 +0300

    did some more course tasks

--- CourseTasks/test.py
+++ CourseTasks/test.py
@@ -25,4 +25,30 @@ def primes():
         x += 1
 
 
-print(list(itertools.takewhile(lambda x: x <= 31, primes())))
+mod_checker = lambda x, mod=0: lambda y: True if y % x == mod else False
+
+
+def check_for_replaces(s, a, b):
+    i = 0
+    while i < 1000:
+        if s.find(a) == -1:
+            return i
+        else:
+            s = s.replace(a, b)
+        i += 1
+    return "Impossible"
+
+
+def check_for_insights(s, t):
+    i = 0
+    a = 0
+    while i < len(s):
+        if s[i:].find(t) == 0:
+            a += 1
+        i += 1
+    return a
+
+
+s = input()
+t = input()
+print(check_for_insights(s, t))
